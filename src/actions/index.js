e8a344d5f2b7630ee3822e916271e2f470bf9eec
Wen Zhang <iwenyou@gmail.com>
2017-08-07 16:24:10 -0700

    error fixed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,16 @@
-const api_key = 'b33507aa5491876f5c6fa855ec19ffad';
+import axios from 'axios';
+
+const API_KEY = 'b33507aa5491876f5c6fa855ec19ffad';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
-  
+  const request = axios.get(url);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index'
 
 export default class SearchBar extends Component {
   constructor(props){
     super(props);
 
-    this.state = {term: '' }
+    this.state = {term: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
   }
