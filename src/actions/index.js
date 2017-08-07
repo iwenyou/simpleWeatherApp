ff390fd6fe2485e4347c94bc041695114d5ad7ab
Wen Zhang <iwenyou@gmail.com>
2017-08-07 16:16:22 -0700

    added rooturl and assigned the api key to it

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 const api_key = 'b33507aa5491876f5c6fa855ec19ffad';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
   return {
     type: FETCH_WEATHER
   }
