011db778d4ea9a12155a22f4fd945521913fad63
Wen Zhang <iwenyou@gmail.com>
2017-08-16 21:46:20 -0700

    google map embed to weather list and stylied to match the chart

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -4,7 +4,7 @@ class GoogleMap extends Component {
   //this will run once the components gets loaded
   componentDidMount() {
     new google.maps.Map(this.refs.map, {
-      zoom:12;
+      zoom:12,
       center: {
         lat: this.props.lat,
         lng: this.props.lon
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -14,7 +14,7 @@ class WeatherList extends Component {
 
     return (
       <tr key={name}>
-        <td>{GoogleMap lon={lon} lat={lat}}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange" units="C"/></td>
         <td><Chart data={pressures} color="green" units="HPA"/></td>
         <td><Chart data={humidities} color="black" units="%"/></td>
