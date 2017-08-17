3742951b84d019d0d6ea2711cd6fd2da47696616
Wen Zhang <iwenyou@gmail.com>
2017-08-16 21:35:26 -0700

    render google inside of this.refs.map

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 
 class GoogleMap extends Component {
-  
+  //this will run once the components gets loaded
+  componentDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom:12;
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon
+      }
+    });
+  }
+  render(){
+    //this.refs.map
+    return <div ref="map" />;
+  }
 }
 
 export default GoogleMap;
